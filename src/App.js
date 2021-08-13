368b07b0117e5b699b6af9002b1e76c34342166a
mrdurden-hub <cmateus297@gmail.com>
2021-08-13 15:11:24 -0300

    separando components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { PostCard } from './components/PostCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Nessa classe é usado Class field, por isso não tem construtor
+  state = {
+    posts: []
+  }
+
+  componentDidMount(){
+    this.loadPosts()
+  }
+
+  loadPosts = async () => {
+    const postResponse = fetch('https://jsonplaceholder.typicode.com/posts')
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
+    
+    const [posts, photos] = await Promise.all([postResponse, photosResponse])
+    
+    const postsJson = await posts.json()
+    const photosJson = await photos.json()
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    })
+
+    this.setState({ posts: postsAndPhotos })
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <section className="container">
+        <div className="posts">
+          {posts.map(post => (
+            <PostCard
+            key={post.id}
+            title={post.title}
+            body={post.body}
+            id={post.body}
+            cover={post.cover}
+            />
+          ))}
+        </div>
+      </section>
+      
+    );
+  }
 }
 
 export default App;
